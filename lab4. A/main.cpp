5f2933abc7839d1133063133b967cd9555412bbb
KseniaShumanskaya <KseniaShumanskaya@gmail.com>
2021-11-08 18:22:29 +0300

    Update main.cpp

--- lab4. A/main.cpp
+++ lab4. A/main.cpp
@@ -4,13 +4,34 @@ using namespace std;
 
 char* gets(char* s) {
 	int a = getchar();
-	int i;
-	for (i = 0; a != '\n'; i++) {
+	int j;
+	for (int i = 0; a != '\n'; i++) {
 		s[i] = a;
 		a = getchar();
+		j = i;
 	}
-	s[i]='\0';
+	s[j + 1] = '\0';
 	return s;
+}
+
+int StrcmpWithTheSameNumberOfCharacter(char* str1, const char* str2) {
+	int n;
+	for (int i = 0; i < strlen(str1); i++) {
+		if (str1[i] == str2[i]) {
+			n = 0;
+		}
+		else {
+			if ((int)str1[i] > (int)str2[i]) {
+				n = 1;
+				break;
+			}
+			if ((int)str1[i] < (int)str2[i]) {
+				n = -1;
+				break;
+			}
+		}
+	}
+	return n;
 }
 
 int _strcmp(char* str1, const char* str2) {
@@ -23,36 +44,18 @@ int _strcmp(char* str1, const char* str2) {
 			k = -1;
 		}
 		else {
-			for (int i = 0; i < strlen(str1); i++) {
-				if (str1[i] == str2[i]) {
-					k = 0;
-				}
-				else {
-					if ((int)str1[i] > (int)str2[i]) {
-						k = 1;
-						break;
-					}
-					if ((int)str1[i] < (int)str2[i]) {
-						k = -1;
-						break;
-					}
-				}
-			}
+			k = StrcmpWithTheSameNumberOfCharacter(str1, str2);
 		}
 	}
 	return k;
 }
 
-int main () {
-	
+int main() {
 	char str1[300];
-    cout << "enter the string 1:" << endl;	
+	cout << "enret the string 1:" << endl;
 	cout << gets(str1) << endl;
-
 	char str2[300];
-	cout << "enter the string 2:" << endl;
+	cout << "enret the string 2:" << endl;
 	cout << gets(str2) << endl;
-
-	cout << _strcmp(str1, str2) << endl;	
+	cout << _strcmp(str1, str2) << endl;
 }
-
